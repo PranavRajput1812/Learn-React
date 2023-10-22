4822f522b79346b0f087d25937467a5c7864fd86
Pranav Rajput <pranavrajput182002@gmail.com>
2023-10-22 16:01:55 +0530

    first commit

---
+++ 01Vitereact/src/App.jsx
@@ -0,0 +1,18 @@
+
+import DogCard from "./DogCard"
+
+
+function App(){
+  return (
+    // <h1>Chai Aur code | Pranav</h1>
+    <>
+    <h1>Dog Detai :</h1>
+    <DogCard/>
+    <DogCard/>
+    
+    </>
+
+  )
+}
+
+export default App
---
+++ 01Vitereact/src/DogCard.jsx
@@ -0,0 +1,9 @@
+function DogCard(){
+    return(
+        <h2>
+            Hi I am Bruno
+        </h2>
+    )
+}
+
+export default DogCard;
